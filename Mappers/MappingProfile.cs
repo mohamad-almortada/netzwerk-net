36ddb3004ce3c6075f8991880bd0a5ca6cf421b8
Mohamad <mo@>
2025-05-26 00:10:44 +0200

    Update MappingProfile

--- Mappers/MappingProfile.cs
+++ Mappers/MappingProfile.cs
@@ -9,12 +9,15 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<User, UsersDto>()
-            .ForMember(dest=> dest.Username, opt=> opt.MapFrom(src=> src.Username))
+            .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Username))
             .ForMember(dest => dest.Email,
                 opt => opt.MapFrom(src =>
-                    src.Email)); 
+                    src.Email));
         CreateMap<MarkerDto, Marker>();
         CreateMap<Marker, MarkerDto>();
-
+        CreateMap<Map, MapDto>();
+        CreateMap<MapDto, Map>();
+        CreateMap<Vote, VoteDto>();
+        CreateMap<VoteDto, Vote>();
     }
 }
